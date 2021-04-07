2f23b2e93b0dcdda948cac287db898b9b31580a9
KY <kyamada20@geffenacademy.ucla.edu>
2021-04-07 08:11:16 -0700

    init

---
+++ test/graphql-express-react/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {ApolloProvider} from 'react-apollo';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import client from './apollo';
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+
+if (module.hot) module.hot.accept()
